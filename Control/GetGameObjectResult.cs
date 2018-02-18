e98a039152725e71ff03dca88e364dc130c52901
caseydedore <caseydedore@gmail.com>
2018-02-18 14:18:59 -0800

    added data struct required by GetGameObjectUnderCursor

---
+++ Control/GetGameObjectResult.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+namespace Assets.Game.Scripts.Control
+{
+    public struct GetGameObjectResult
+    {
+        public GameObject GameObjectResult { get; private set; }
+        public Vector3 ContactPoint { get; private set; }
+
+
+        public GetGameObjectResult(GameObject gameObject, Vector3 contactPoint)
+        {
+            GameObjectResult = gameObject;
+            ContactPoint = contactPoint;
+        }
+    }
+}
