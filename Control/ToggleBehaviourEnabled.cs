ce8abd7f41364b8e20b1ea7447ba6e6bbbea94f8
caseydedore <caseydedore@gmail.com>
2018-02-18 21:41:45 -0800

    classes

---
+++ Control/ToggleBehaviourEnabled.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+namespace Control
+{
+    public class ToggleBehaviourEnabled : MonoBehaviour
+    {
+        [SerializeField]
+        private MonoBehaviour target = null;
+        [SerializeField]
+        private bool isEnabledAtStart = true;
+
+        void Start()
+        {
+            target.enabled = isEnabledAtStart;
+        }
+
+        public void Toggle()
+        {
+            target.enabled = !target.enabled;
+        }
+    }
+}
---
+++ Control/ToggleCursor.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+namespace Control
+{
+    public class ToggleCursor : MonoBehaviour
+    {
+        [SerializeField]
+        private bool isVisibleAtStart = true;
+
+
+        public void Start()
+        {
+            Cursor.visible = isVisibleAtStart;
+        }
+
+        public void Toggle()
+        {
+            Cursor.visible = !Cursor.visible;
+        }
+    }
+}
