aacb830a1b5b4d8c0219e862add23daf87804385
caseydedore <caseydedore@gmail.com>
2018-04-10 21:31:47 -0700

    object finder

---
+++ Sensor/ObjectFinder.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Sensor
+{
+    public class ObjectFinder
+    {
+        public T[] GetComponents<T>(Vector3 origin, float radius) where T : Component
+        {
+            var defaultLayerMask = 1 << LayerMask.NameToLayer("Default");
+            return GetComponents<T>(origin, radius, defaultLayerMask);
+        }
+
+        public T[] GetComponents<T>(Vector3 origin, float radius, LayerMask mask) where T : Component
+        {
+            var colliders = Physics.OverlapSphere(origin, radius, mask);
+            var filtered = 
+                colliders
+                .Select(collider => collider.GetComponent<T>())
+                .Where(component => component != null).ToArray();
+            return filtered;
+        }
+    }
+}
